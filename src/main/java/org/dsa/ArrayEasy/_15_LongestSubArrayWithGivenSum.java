2fdf515608fa0488bb1478c0457591445cf2e706
sumitbytes01 <sumitbytes01@gmail.com>
2026-05-04 16:32:39 +0530

    revision of easy array

--- src/main/java/org/dsa/ArrayEasy/_15_LongestSubArrayWithGivenSum.java
+++ src/main/java/org/dsa/ArrayEasy/_15_LongestSubArrayWithGivenSum.java
@@ -2,30 +2,31 @@ package org.dsa.ArrayEasy;
 
 public class _15_LongestSubArrayWithGivenSum {
     public static void main(String[] args) {
-        int gSum = 10;
-        int[] array = {2,3,5,1,9};
+        int gSum = 8;
+        int[] array = {2, 3, 5, 1, 9};
         int n = array.length;
         // 4 approaches
         // 1 find every subarray
         // 2 optimize find all subarrays approach
         // 3 prefix sum
         // 4 two pointer
-        int sum = array[0];
-        int max = 0;
-        int start = 0;
         int end = 0;
-        while(end <n){
-            while(sum>gSum && end>start){
+        int start = 0;
+        int sum = array[0];
+        int maxLen = 0;
+        while(end<n){
+            while(end<n && sum>gSum){
                 sum = sum - array[start];
                 start++;
             }
-            if(sum == gSum){
-                max = Math.max(max, end-start+1);
+            if(sum==gSum){
+                maxLen = Math.max(maxLen, end-start+1);
+                System.out.println(start+"-"+end+" Len: "+maxLen);
             }
             end++;
-            if (end<n)
-                sum = sum+array[end];
+            if(end<n){
+                sum = sum + array[end];
+            }
         }
-        System.out.println(max);
     }
 }
