ef61a989da71ff54a0a6ccc763087835b8aa7989
sumitbytes01 <sumitbytes01@gmail.com>
2026-07-08 18:16:09 +0530

    Allocate books binary search for answers-optimal

--- src/main/java/org/dsa/binaryAnswers/_9_AllocateBooks.java
+++ src/main/java/org/dsa/binaryAnswers/_9_AllocateBooks.java
@@ -6,7 +6,30 @@ public class _9_AllocateBooks {
     static void main() {
         int[] books = {12, 34, 67, 90};
         int students = 2;
+        /*Method
+        Time Complexity
+        Brute Force
+        O((sum-max) × n)
+        Binary Search
+        O(n log(sum-max))*/
         System.out.println(bruteAllocate(books, students));
+        System.out.println(optimalAllocate(books, students));
+    }
+
+    private static int optimalAllocate(int[] books, int students) {
+        if (students > books.length)
+            return -1;
+        int min = Arrays.stream(books).max().getAsInt();
+        int max = Arrays.stream(books).sum();
+        while(min<=max){
+            int mid = min + (max-min)/2;
+            boolean isPossible = isPossible(books, students, mid);
+            if(isPossible){
+                max = mid-1;
+            }
+            else min = mid+1;
+        }
+        return min;
     }
 
     private static int bruteAllocate(int[] books, int students) {
